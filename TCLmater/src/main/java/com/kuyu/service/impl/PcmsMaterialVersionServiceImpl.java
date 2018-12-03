d798424f06d246b9d0332b4e49e141d66ced1d85
zhaiyule <123@qq.com>
2018-12-03 10:04:06 +0800

    物料上传

--- TCLmater/src/main/java/com/kuyu/service/impl/PcmsMaterialVersionServiceImpl.java
+++ TCLmater/src/main/java/com/kuyu/service/impl/PcmsMaterialVersionServiceImpl.java
@@ -65,6 +65,10 @@ public class PcmsMaterialVersionServiceImpl extends ServiceImpl<PcmsMaterialVers
         File newFile = new File(finalPath);
         file.transferTo(newFile);
         Map<String, String> map  = readExcel(finalPath);
+        if(map.size() == 0 || null == map){
+            newFile.delete();
+            throw new ParamException("数据为空");
+        }
         PcmsSupplierMaterialModel psmm = pcmsSupplierMaterialMapper.findSupplierMaterialByVendorAndCompany(vendor_id,"555"/*,userInfo.getEmployeeModel().getCompany()*/);
         PcmsMaterialVersionModel pcmsMaterialVersionModel = new PcmsMaterialVersionModel();
         if(null != psmm){
@@ -157,9 +161,6 @@ public class PcmsMaterialVersionServiceImpl extends ServiceImpl<PcmsMaterialVers
 
             // 获取到Excel文件中的所有行数
             int rows = sheet.getPhysicalNumberOfRows();
-            if(rows == 0){
-                throw new ParamException(ResultVoUtils.fail("文件为空"));
-            }
             int max_cells = 0;
             // 获取最长的列，在实践中发现如果列中间有空值的话，那么读到空值的地方就停止了。所以我们需要取得最长的列。
             //如果每个列正好都有一个空值得话，通过这种方式获得的列长会比真实的列要少一列。所以我自己会在将要倒入数据库中的EXCEL表加一个表头
@@ -240,6 +241,8 @@ public class PcmsMaterialVersionServiceImpl extends ServiceImpl<PcmsMaterialVers
                 }
             }
         } catch (Exception e) {
+            File newFile = new File(filePath);
+            newFile.delete();
             e.printStackTrace();
         }
         return map;
