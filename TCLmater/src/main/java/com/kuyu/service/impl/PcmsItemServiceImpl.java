bc90ed29f05156040a7921ca3c1dfa03b7562b02
991530643 <991530643@qq.com>
2018-12-29 17:40:43 +0800

    --

--- TCLmater/src/main/java/com/kuyu/service/impl/PcmsItemServiceImpl.java
+++ TCLmater/src/main/java/com/kuyu/service/impl/PcmsItemServiceImpl.java
@@ -283,15 +283,11 @@ public class PcmsItemServiceImpl implements PcmsItemService{
 	//0:未接单 1:已接单制作中 2待验收 3待结算 4已驳回 5结算中 6结算失败 7已结算 -1已作废
 	@Override
 //	public ResultVo changeItemStatus(Integer itid,Integer status,String reason) {
-	public ResultVo changeItemStatus(Integer itid, Integer status, LoginUserInfo userInfo,String context) {
+	public ResultVo changeItemStatus(Integer itid, Integer status, LoginUserInfo userInfo,String context,LoginUserInfo user) {
 		//TODO
 		//检测是否有权限操作
+		String role=user.getUserRole();
 		
-		
-		if(status==5){
-			//TODO
-			//通知共享系统处理结算
-		}
 		
 		 //增加立项单日志
 	    PcmsItemLog log=new PcmsItemLog();
@@ -897,6 +893,57 @@ public class PcmsItemServiceImpl implements PcmsItemService{
 		}
 		
 		return 0;
+	}
+
+
+
+	@Override
+	public void checkPaymentDetail(String synDate, int i) throws ClientProtocolException, IOException {
+		// 获取默认的请求客户端
+		CloseableHttpClient client = HttpClients.createDefault();
+		// 通过HttpPost来发送post请求
+		HttpPost httpPost = new HttpPost(queryPaymentDetailUrl);
+
+		/*
+		 * post带参数开始
+		 */
+		// 第三步：构造list集合，往里面丢数据
+
+		JSONObject param = new JSONObject();
+		// param.put("FSSC_BILL", fsscBill);
+		param.put("FINANCIAL_DATE", synDate);
+		param.put("FROM_INDEX", 1);
+		param.put("END_INDEX", 1000);
+
+		List<NameValuePair> list = new ArrayList<NameValuePair>();
+		BasicNameValuePair basicNameValuePair = new BasicNameValuePair("requestParams", param.toString());
+		list.add(basicNameValuePair);
+		// 第二步：我们发现Entity是一个接口，所以只能找实现类，发现实现类又需要一个集合，集合的泛型是NameValuePair类型
+		UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(list, "UTF-8");
+		// 第一步：通过setEntity 将我们的entity对象传递过去
+		httpPost.setEntity(formEntity);
+		/*
+		 * post带参数结束
+		 */
+		// HttpEntity
+		// 是一个中间的桥梁，在httpClient里面，是连接我们的请求与响应的一个中间桥梁，所有的请求参数都是通过HttpEntity携带过去的
+		// 通过client来执行请求，获取一个响应结果
+		CloseableHttpResponse response = client.execute(httpPost);
+		HttpEntity entity = response.getEntity();
+		String str = EntityUtils.toString(entity, "UTF-8");
+		// 关闭
+		response.close();
+
+		JSONObject result = JSON.parseObject(str);
+
+		PcmsPaymentCheck check = new PcmsPaymentCheck();
+		check.setCheckDate(synDate);
+		check.setCheckTime(new Date());
+		check.setCheckType(i);
+		check.setResultJson(str);
+
+		System.out.println(result);
+
 	}
 
 	
